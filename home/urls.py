e36cb0e7bf2f9750045b4721ba99cc866f4c8445
FranLop24 <franlopbri@gmail.com>
2025-12-03 13:37:25 -0600

    02 Add statics for index page

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('me/', views.personal_page, name='me')
 ]
--- home/views.py
+++ home/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home/home.html')
+
+def personal_page(request):
+    return render(request, 'home/index.html')
